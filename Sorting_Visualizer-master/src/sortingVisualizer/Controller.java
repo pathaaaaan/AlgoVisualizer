431ec6c5c8d63194176f2e94060f4c783af68d53
pathaaaaan <friendlyhasnainkhan@gmail.com>
2025-09-20 22:24:43 +0530

    Update about section to reflect team development

--- Sorting_Visualizer-master/src/sortingVisualizer/Controller.java
+++ Sorting_Visualizer-master/src/sortingVisualizer/Controller.java
@@ -87,7 +87,7 @@ public final class Controller implements Initializable {
         aboutBtn.setOnMouseClicked(e -> {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("About Application & Developer");
-            alert.setHeaderText("Developed by Arpit Singh SIkarwar");
+            alert.setHeaderText("Developed by Team ALGOVIZ");
             alert.setContentText(
                     "Visualize Bubble Sort Algorithm, Insertion Sort Algorithm, Selection Sort Algorithm, Shell Sort, Quick Sort Algorithm & Merge Sort Algorithm and see how these work.\n\nTip: If size of array is larger make sure that you decrease the delay time!");
             alert.show();
